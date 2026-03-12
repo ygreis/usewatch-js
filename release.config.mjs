b65f4cedb13d794242d619effd55826f228519d3
Ygreis <gabrielyallison2012@hotmail.com>
2026-03-12 18:40:09 -0300

    chore(docs): update library documentation and usage examples

--- release.config.mjs
+++ release.config.mjs
@@ -1,7 +1,20 @@
 export default {
   branches: ["main", "master"],
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+        releaseRules: [
+          { breaking: true, release: "major" },
+          { type: "feat", release: "minor" },
+          { type: "chore", release: "minor" },
+          { type: "fix", release: "patch" },
+          { type: "perf", release: "patch" },
+          { type: "docs", release: "patch" },
+        ],
+      },
+    ],
     "@semantic-release/release-notes-generator",
     [
       "@semantic-release/changelog",
